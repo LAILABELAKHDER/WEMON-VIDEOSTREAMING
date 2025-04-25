476e8a2892744ea5bb383b2d60a9c1422413b252
LAILABELAKHDER <leilabelakhder@gmail.com>
2025-04-25 23:40:21 +0200

    Premier commit : ajout du projet

---
+++ blog_app/index.js
@@ -0,0 +1,19 @@
+// blog_app/index.js
+import express from "express";
+import connectDB from "./config/db.js";
+import ArticleRouter from "./routes/ArticleRouter.js";
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", ArticleRouter);
+connectDB();
+
+app.get("/", (request, response) => {
+  response.send({ message: "Hello from an Express API!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
